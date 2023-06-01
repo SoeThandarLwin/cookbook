52833c18df59643bb652bd01375fdac97004a3e3
Soe Thandar Lwin <soethandar.lwin@kmutt.ac.th>
2023-06-01 19:20:32 +0700

    Add tailwind and build login page scaffold

--- cookbook-frontend/src/main.jsx
+++ cookbook-frontend/src/main.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.jsx';
 import GlobalContextProvider from './contexts/GlobalContextProvider.jsx';
+
+import App from './App.jsx';
+import Login from './pages/Login.jsx';
+
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
 ]);
 
