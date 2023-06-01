4111c75f2194f86c9338065cbcceb3c2d1927795
Soe Thandar Lwin <soethandar.lwin@kmutt.ac.th>
2023-06-01 23:31:41 +0700

    Format code

--- cookbook-backend/src/index.js
+++ cookbook-backend/src/index.js
@@ -3,12 +3,12 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 
-import rootHandler from './handlers/rootHandler.js';
-import registerHandler from './handlers/registerHandler.js';
-import loginHandler from './handlers/loginHandler.js';
 import createRecipeHandler from './handlers/createRecipeHandler.js';
-import listRecipesHandler from './handlers/listRecipesHandler.js';
 import getRecipeHandler from "./handlers/getRecipeHandler.js";
+import listRecipesHandler from './handlers/listRecipesHandler.js';
+import loginHandler from './handlers/loginHandler.js';
+import registerHandler from './handlers/registerHandler.js';
+import rootHandler from './handlers/rootHandler.js';
 
 const app = express();
 const port = 3000;
@@ -29,5 +29,5 @@ app.get('/recipes', listRecipesHandler);
 app.get('/recipes/:id', getRecipeHandler);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Cookbook app listening at http://localhost:${port}`);
 });
