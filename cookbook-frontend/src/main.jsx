8fa79b99bf7e4660cfb2e4b516a5eae64a3b0b13
Soe Thandar Lwin <soethandar.lwin@kmutt.ac.th>
2023-06-01 18:32:40 +0700

    Remove default code

--- cookbook-frontend/src/main.jsx
+++ cookbook-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App.jsx';
-import './index.css';
 import GlobalContextProvider from './contexts/GlobalContextProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
