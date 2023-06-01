ab0dee4b9a61b648d460a3ac8a900fdab23a3b38
Soe Thandar Lwin <soethandar.lwin@kmutt.ac.th>
2023-06-01 15:59:17 +0700

    Add global context

---
+++ cookbook-frontend/src/contexts/GlobalContextProvider.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import GlobalContext from "./GlobalContext.js";
+
+const GlobalContextProvider = (props) => {
+  const [profile, setProfile] = useState({});
+
+  const globalState = {
+    profile,
+    setProfile,
+  };
+
+  return (
+    <GlobalContext.Provider value={globalState}>
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalContextProvider;
