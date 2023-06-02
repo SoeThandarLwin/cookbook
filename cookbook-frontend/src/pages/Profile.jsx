07111f06d5a0dbe362d9d3423c631d81c097463c
Soe Thandar Lwin <soethandar.lwin@kmutt.ac.th>
2023-06-02 18:48:28 +0700

    Refactor profile page

--- cookbook-frontend/src/pages/Profile.jsx
+++ cookbook-frontend/src/pages/Profile.jsx
@@ -1,7 +1,23 @@
 import { useNavigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import GlobalContext from '../contexts/GlobalContext.js';
+import { useQuery } from 'react-query';
+import Axios from '../utils/Axios.js';
 
 export default function Profile() {
+  const [avatar, setAvatar] = useState('');
+
   const navigate = useNavigate();
+
+  useQuery(['profile'], () => {
+    Axios.get('profile').then((res) => {
+      setAvatar(res.data.avatar);
+    });
+  });
+
+  const getAvatar = () => {
+    return `http://localhost:3000/images/${avatar ? avatar : 'avatar.png'}`;
+  };
 
   const handleLogout = () => {};
 
@@ -19,6 +35,8 @@ export default function Profile() {
 
   return (
     <div>
+      <img src={getAvatar()} />
+
       <p>Log Out</p>
 
       <p>Change Password</p>
