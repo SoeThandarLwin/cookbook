cdf47d89ccc7cae86c1cffbd2a6f194fc03ab903
Soe Thandar Lwin <soethandar.lwin@kmutt.ac.th>
2023-06-02 14:31:51 +0700

    Refactor listRecipesHandler

--- cookbook-backend/src/handlers/listRecipesHandler.js
+++ cookbook-backend/src/handlers/listRecipesHandler.js
@@ -2,7 +2,11 @@ import pool from '../data/database.js';
 
 const listRecipesHandler = async (request, response) => {
   const [rows] = await pool.execute(
-    'SELECT recipes.id, recipes.name, recipes.image, users.id as user_id, users.name as user_name FROM recipes JOIN users ON users.id = recipes.user_id',
+    `SELECT recipes.id, recipes.name, recipes.image, recipes.calories, recipes.prep_time, 
+            users.id as user_id, users.name as user_name 
+        FROM recipes 
+        JOIN users 
+        ON users.id = recipes.user_id`,
     [],
   );
 
@@ -10,13 +14,15 @@ const listRecipesHandler = async (request, response) => {
     id: row.id,
     name: row.name,
     image: row.image,
+    calories: row.calories,
+    prep_time: row.prep_time,
     user: {
       id: row.user_id,
       name: row.user_name,
     },
   }));
 
-  response.status(200).send({data: recipes});
+  response.status(200).send({ data: recipes });
 };
 
 export default listRecipesHandler;
